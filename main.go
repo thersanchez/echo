896bc8ec1fdee5289bfaf11440afac6c1cfbcbc4
Esther Sanchez <sanchez.ther@gmail.com>
2017-06-17 17:45:34 +0200

    simplify loop by using strings.Join

--- main.go
+++ main.go
@@ -3,14 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	sep := "" // we (sep)arate each string with a space, but not the first time.
-	for _, v := range os.Args[1:] {
-		fmt.Print(sep)
-		fmt.Print(v)
-		sep = " "
-	}
-	fmt.Println()
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
